83c9b94b872cd9684ce462e2ee9b9ccf0e39d65d
sinisa229 <sinisa229@gmail.com>
2020-09-23 21:46:59 +0100

    Added testcontainers

--- src/test/java/com/selenium/playground/SeleniumPlaygroundApplicationTests.java
+++ src/test/java/com/selenium/playground/SeleniumPlaygroundApplicationTests.java
@@ -1,11 +1,16 @@
 package com.selenium.playground;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,6 +18,14 @@ public class SeleniumPlaygroundApplicationTests {
 
 	@Autowired
 	private DriverProvider driverProvider;
+
+	@Rule
+	public DockerComposeContainer environment =
+			new DockerComposeContainer(new File("docker-compose.yml"))
+					.withExposedService("selenium-hub", 4444)
+					.withExposedService("chrome", 5555)
+					.withExposedService("firefox", 5555)
+					.withLocalCompose(false);
 
 	@Test
 	public void contextLoads() {
