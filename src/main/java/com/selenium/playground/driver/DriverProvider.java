b38d7d71bad0caa08ee6fb32f785ba7cacfb43d5
SMihajlovski <sinisha.mihajlovski@esure.com>
2021-10-03 16:26:25 +0100

    Clean up

--- src/main/java/com/selenium/playground/driver/DriverProvider.java
+++ src/main/java/com/selenium/playground/driver/DriverProvider.java
@@ -1,9 +1,7 @@
 package com.selenium.playground.driver;
 
-import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 
 public interface DriverProvider {
-    @SneakyThrows
     WebDriver getWebDriver();
 }
